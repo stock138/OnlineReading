31d3930e5f784583e0cc357eb78fd9aeb206f3bc
gongwenhai <gongwenhai@csii.com.cn>
2024-12-11 09:18:27 +0800

    test分支提交，cherry-pick新增文件测试

---
+++ Model-Web/src/main/java/com/cn/lucky/morning/model/web/controller/admin/a.java
@@ -0,0 +1,13 @@
+package com.cn.lucky.morning.model.web.controller.admin;
+
+/**
+ * @program: OnlineReading
+ * @description:
+ * @author: gongwenhai
+ * @create: 2024-12-11 09:17
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+}
